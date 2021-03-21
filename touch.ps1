a69a026752aa4c49187e951bf152e32120e9e28e
R.V.Vivek <rvvaghu@hotmail.com>
2021-03-21 22:01:49 +0530

    Created touch.ps1 File

---
+++ touch.ps1
@@ -0,0 +1,14 @@
+
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
